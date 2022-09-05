7c44ccaf33cd4ab18952045a16f74711ef1d52bf
programacasaverdeeamarela <lorenzocosta.kallas@gmail.com>
2022-09-05 15:43:58 -0300

    teste

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Form from "./Components/Form";
+
+import React from "react";
+
+function App() {
+  return (
+    <>
+    <Form />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Form.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Form = ()=>{
+    return(
+    <>
+    <h1>cadastro casa verde e amarela
+        
+    </h1>
+    </>
+    )
+}
+
+export default Form
